7040cfc09203ce5fa492d54ebe89b9c077bd1249
Michael Dear <michaelcdear@gmail.com>
2022-07-03 11:11:32 -0600

    commit tree 05b967ad3aa3a74430d6a60e209010d7c8b8db71

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
+
 const init = () => {
-  
+    let form = document.getElementById("form")
+    form.addEventListener("submit", e => {
+        let textInput = e.target.searchByID.value
+        e.preventDefault()
+        console.log("Text Input:", textInput)
+        form.reset()
+        fetchData(textInput)
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+function fetchData(input) {
+    return fetch("http://127.0.0.1:5500/db.json")
+          .then(resp => resp.json())
+          .then (data => processData(data, input))
+}
+
+function processData(data, input) {
+    console.log("Data:", data.movies[input-1])
+    document.getElementById("title").textContent = data.movies[input-1].title
+    document.getElementById("summary").textContent = data.movies[input-1].summary
+}
